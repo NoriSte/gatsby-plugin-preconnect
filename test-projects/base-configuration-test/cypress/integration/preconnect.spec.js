0f05a0c22e5c931cdd3f756c1ec87506389c93ef
NoriSte <nori.ste.magni@gmail.com>
2019-05-13 17:44:12 +0200

    test: 💍 Test that the tags are added too

--- test-projects/base-configuration-test/cypress/integration/preconnect.spec.js
+++ test-projects/base-configuration-test/cypress/integration/preconnect.spec.js
@@ -4,12 +4,15 @@ context('Preconnect', () => {
   it('A preconnect link should be added for every specified domain', () => {
     cy.visit('http://localhost:9000')
     cy.get("link").then($el => {
+      let tagFound = false;
       $el.each((i, el) => {
         const href = el.getAttribute("href");
         if(el.nodeName.toLowerCase() === "link" && (href.includes("https://foo.com") || href.includes("https://bar.com"))) {
+          tagFound = true;
           expect(el.getAttribute("rel")).to.equal("preconnect");
         }
       });
+      expect(tagFound).to.equal(true);
     })
   })
 })
