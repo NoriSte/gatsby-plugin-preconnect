9fa8ae73bf39123b8e7a5dfb430bb2300da1ad3c
schnogz <andrew@blockchain.com>
2020-04-09 19:48:15 -0400

    feat(cors): add crossorigin by default and further customization support :sparkles:

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -1,28 +1,47 @@
 import React from "react";
 
+export const onRenderBody = ({setHeadComponents}, pluginOptions) => {
+  const {domains: domainList} = pluginOptions;
 
-export const onRenderBody = ({
-  setHeadComponents,
-}, pluginOptions) => {
-
-  if(!pluginOptions || !pluginOptions.domains) {
+  if(!pluginOptions || !domainList) {
     throw new Error("gatsby-plugin-preconnect: Missing `options.domains`");
   }
-  if(!Array.isArray(pluginOptions.domains)) {
+
+  if(!Array.isArray(domainList)) {
     throw new Error("gatsby-plugin-preconnect: `options.domains` is not an array");
   }
-  if(pluginOptions.domains.find(el => typeof el !== "string")) {
-    throw new Error("gatsby-plugin-preconnect: `options.domains` only accept strings");
-  }
+
+  domainList.length > 0 && domainList.forEach(domainConfig => {
+    if (typeof domainConfig !== "string" && typeof domainConfig !== "object") {
+      throw new Error("gatsby-plugin-preconnect: domains in `options.domains` array must be either a string or object");
+    }
+    if (typeof domainConfig === "object") {
+      const { crossOrigin, domain } = domainConfig;
+      if (typeof domain !== "string") {
+        throw new Error("gatsby-plugin-preconnect: `domain` in domain config object must be a string");
+      }
+      if (crossOrigin !== "use-credentials" && crossOrigin !== false) {
+        throw new Error("gatsby-plugin-preconnect: `crossOrigin` in domain config object must be either false or `use-credentials`");
+      }
+    }
+  })
 
   // remove duplicate values
-  const domains =  Array.from(new Set(pluginOptions.domains));
+  const domains = Array.from(new Set(domainList));
+
+  const createLinkEl = (domain, crossOrigin) => {
+    return React.createElement("link", {
+      crossOrigin: crossOrigin,
+      href: domain,
+      key: domain,
+      rel: "preconnect",
+    });
+  };
 
   setHeadComponents(
-    domains.map(domain => React.createElement('link', {
-      rel: "preconnect",
-      href: domain,
-      key: domain
-    })));
-
+    domains.map(domainConfig => {
+      return typeof domainConfig === "object"
+        ? createLinkEl(domainConfig.domain, domainConfig.crossOrigin)
+        : createLinkEl(domainConfig, "");
+    }));
 }
