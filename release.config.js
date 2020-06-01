34ec60789fbfca73d03ee795ae1be3db61cfe519
Stefano Magni <nori.ste.magni@gmail.com>
2020-06-01 07:45:46 +0200

    Revert "chore: avoid release for patch deps updates"
    This reverts commit 955842227198368bb5c9cc14ffe7d29619de20be.

--- release.config.js
+++ release.config.js
@@ -1,14 +1,7 @@
 module.exports = {
   plugins: [
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/github',
-    '@semantic-release/npm',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['package.json'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
-  ],
-}
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+    "@semantic-release/npm"
+  ]
+};
