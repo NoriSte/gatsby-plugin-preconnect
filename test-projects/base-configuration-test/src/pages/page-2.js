a4cba10a9df5c8e81a7136f209cc156f7d011a39
Stefano Magni <nori.ste.magni@gmail.com>
2021-03-07 14:00:17 +0100

    chore(deps): update Gatsby to the v3

--- test-projects/base-configuration-test/src/pages/page-2.js
+++ test-projects/base-configuration-test/src/pages/page-2.js
@@ -1,8 +1,8 @@
-import { Link } from "gatsby";
-import React from "react";
-import Layout from "../components/layout";
-import SEO from "../components/seo";
+import * as React from "react"
+import { Link } from "gatsby"
 
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
